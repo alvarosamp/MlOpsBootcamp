dbb1e6c96349d16f5c7c62fc84d26c9b1dfc7940
AlvaroCareli <122116260+alvarosamp@users.noreply.github.com>
2025-04-06 17:36:30 -0300

    Testes

--- MlFlow/demo.py
+++ MlFlow/demo.py
@@ -4,32 +4,28 @@ import argparse
 import time
 
 
-
-def eval(p1,p2):
+def eval(p1, p2):
     output_metric = p1**2 + p2**2
     return output_metric
 
-
 def main(inp1, inp2):
+    mlflow.set_experiment("DecisionTreeClass")
+    #with mlflow.start_run(run_name='Example Demo'):
     with mlflow.start_run():
-        mlflow.set_tag("version", "1.0.0")
-        mlflow.log_param("input1", inp1)
-        mlflow.log_param("input2", inp2)
-        mlflow.log_metric("accuracy", 0.9)
-    
+        mlflow.set_tag("version","1.0.0")
+        mlflow.log_param("param1",inp1)
+        mlflow.log_param("param2",inp2) # key, value
+        metric = eval(p1 = inp1, p2 = inp2)
+        mlflow.log_metric("Eval_Metric",metric)
+        os.makedirs("dummy", exist_ok=True)
+        with open("dummy/example.txt", "wt") as f:
+            f.write(f"Artifact created at {time.asctime()}")
+        mlflow.log_artifacts("dummy")
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input1", type=float, default=1.0, help="First input parameter")
-    parser.add_argument("--input2", type=float, default=1.0, help="Second input parameter")
-    args = parser.parse_args()  # Corrigir aqui para processar os argumentos
-
-    metric = eval(args.input1, args.input2)  # Acessar os argumentos corretamente
-    mlflow.log_metric("metric", metric)
-    os.makedirs("dummy", exist_ok=True)
-    with open("dummy/metric.txt", "w") as f:
-        f.write(f'Metric: {metric}')
-    mlflow.log_artifact("dummy/metric.txt")
-
-
-    #Parsed_args.param1
+    args = argparse.ArgumentParser()
+    args.add_argument("--param1","-p1", type=int, default=5)
+    args.add_argument("--param2","-p2", type=int, default=10)
+    parsed_args = args.parse_args()
+    main(parsed_args.param1, parsed_args.param2)
